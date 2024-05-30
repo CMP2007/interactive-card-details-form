c3d11e209954094cee082aeaec8202aca64a990b
PucheCarlos <puchecarlos00@gmail.com>
2024-05-30 02:22:31 -0400

    revision pendiente

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const YY = document.getElementById("YY");
 const cvc = document.getElementById("cvc");
 let id;
 
+const borderInput1 = document.getElementById("divInputBig1");
+const borderInput2 = document.getElementById("divInputBig2");
+const borderInput3 = document.querySelector(".borderInputSmall1");
+const borderInput4 = document.querySelector(".borderInputSmall2");
+const borderInput5 = document.querySelector(".borderInputMed");
 
 document.addEventListener("click", (event)=>{
     getInput(event)
@@ -18,8 +23,64 @@ document.addEventListener("click", (event)=>{
     getNumber()
     getMonth()
     getYear()
-    getCvc()
+    getCvc()  
 })
+
+/***************************************** event listeners border ************************************************** */
+
+input1.addEventListener("focus",()=>{
+    borderInput1.style.setProperty("--visibleNumber", "inline")
+})
+input1.addEventListener("blur",()=>{
+    if (input1.value) {
+        nameOk = true;
+    }
+    else { 
+        nameOk = false;
+    }
+    borderInput1.style.setProperty("--visibleNumber", "none")
+})
+
+
+input2.addEventListener("focus",()=>{
+    borderInput2.style.setProperty("--visibleNumber", "inline")
+})
+input2.addEventListener("blur",()=>{
+    if (numberOk == true && input2.value.length > 15 || input2.value.length == 0) {
+        borderInput2.style.setProperty("--visibleNumber", "none")
+    }
+})
+
+
+input3.addEventListener("focus",()=>{
+    borderInput3.style.setProperty("--visibleNumber", "inline")
+})
+input3.addEventListener("blur",()=>{
+    if (monthOk == true && input3.value.length == 2 || input3.value.length == 0) {
+        borderInput3.style.setProperty("--visibleNumber", "none")
+    }
+})
+
+
+input4.addEventListener("focus",()=>{
+    borderInput4.style.setProperty("--visibleNumber", "inline")
+})
+input4.addEventListener("blur",()=>{
+    if (yearOk == true && input4.value.length == 2 || input4.value.length == 0) {
+        borderInput4.style.setProperty("--visibleNumber", "none")
+    }
+})
+
+
+input5.addEventListener("focus",()=>{
+    borderInput5.style.setProperty("--visibleNumber", "inline")
+})
+input5.addEventListener("blur",()=>{
+    if (cvcOk == true && input5.value.length == 3 || input5.value.length == 0) {
+        borderInput5.style.setProperty("--visibleNumber", "none")   
+    }
+})
+
 
 function getInput(event) {
     let input = event.target.id
@@ -36,8 +97,8 @@ function getName() {
             let name = input1.value;
             name = name.toUpperCase();
             cardName.innerHTML = name;
+            validateName();
         })
-        console.log(id);
     }
 }
 
@@ -49,6 +110,7 @@ function getNumber() {
             let subNumber = formNumber.match(new RegExp('.{1,' + 4 + '}', 'g'));
             let cardNumberValue = subNumber.join(" ");
             cardNumber.innerHTML = cardNumberValue;
+            validateNumber(number);
         })
         console.log(id);
     }
@@ -58,7 +120,9 @@ function getMonth() {
     if (id == "3") {
         input3.addEventListener("input", ()=>{
             let month = input3.value;
-            MM.innerHTML = month;
+            let formMonth = month.padEnd(2, 0)
+            MM.innerHTML = formMonth;
+            validateMonth(month)
         })
     }
 }
@@ -67,7 +131,9 @@ function getYear() {
     if (id == "4") {
         input4.addEventListener("input", ()=>{
             let year = input4.value;
-            YY.innerHTML = year;
+            let formYear = year.padEnd(2, 0);
+            YY.innerHTML = formYear;
+            validateYear(year);
         })
     }
 }
@@ -76,8 +142,136 @@ function getCvc() {
     if (id == "5") {
         input5.addEventListener("input", ()=>{
             let cvcValue = input5.value;
-            cvc.innerHTML = cvcValue;
+            let formCvc = cvcValue.padEnd(3, 0);
+            cvc.innerHTML = formCvc;
+            validateCvc(cvcValue)
         })
     }
 }
-/******************************************************************************************* */
+
+/********************************************* validate data ********************************************** */
+let alert1 = document.querySelector(".alert1");
+let alert2 = document.querySelector(".alert2");
+let alert3 = document.querySelector(".alert3");
+let nameOk = false
+let numberOk = false;
+let monthOk = false;
+let yearOk = false
+let cvcOk = false;
+
+function validateName() {
+    if (input1.value) {
+        borderInput1.style.setProperty("--borderColor", " linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))");
+    }
+}
+
+function validateNumber(number) {
+    let numberCheck =  Number(number)
+        if (!isNaN(numberCheck) && input2.value.length) {
+            numberOk = true;
+            alert1.style.setProperty("display", "none");
+            borderInput2.style.setProperty("--borderColor", " linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))");
+        }
+        else{
+            numberOk = false;
+            alert1.style.setProperty("display", "block");
+            borderInput2.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+        }
+}
+
+function validateMonth(month) {
+    let monthCheck = Number(month);
+    if (!isNaN(monthCheck) && input3.value.length) {
+        monthOk = true;
+        alert2.style.setProperty("display", "none");
+        borderInput3.style.setProperty("--borderColor", " linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))");
+    }
+    else{
+        monthOk = false;
+        borderInput3.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+    }
+}
+
+function validateYear(year) {
+    let yearCheck = Number(year);
+    if (!isNaN(yearCheck) && input4.value.length) {
+        yearOk = true;
+        alert2.style.setProperty("display", "none");
+        borderInput4.style.setProperty("--borderColor", " linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))");
+    }
+    else{
+        yearOk = false;
+        borderInput4.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+    }
+}
+
+function validateCvc(cvcValue) {
+    let cvcCheck = Number(cvcValue);
+    if (!isNaN(cvcCheck) && input5.value.length) {
+        cvcOk = true;
+        alert3.style.setProperty("display", "none");
+        borderInput5.style.setProperty("--borderColor", " linear-gradient(to right, hsl(249, 99%, 64%), hsl(278, 94%, 30%))");
+        console.log(cvcOk);
+    }
+    else{
+        cvcOk = false;
+        borderInput5.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+        console.log(cvcOk)
+    }
+}
+
+function alertLetters(letters) {
+    
+}
+/********************************************* buttons ********************************************** */
+const button = document.querySelector(".button");
+const form = document.querySelector(".sect_2");
+const thanks = document.querySelector(".sect_3");
+
+button.addEventListener("click", ()=>{
+    if (nameOk && numberOk && monthOk && yearOk && cvcOk) {
+        form.classList.add("formOff");
+        thanks.classList.remove("thanksOff");
+    }
+    else{
+        let errorData = [nameOk, numberOk, monthOk, yearOk, cvcOk];
+        errorData.forEach(function(data) {
+            let cont = errorData.findIndex(value => !value);
+            
+            switch (cont){
+                case 0 :
+                    borderInput1.style.setProperty("--visibleNumber", "inline");
+                    borderInput1.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+                    break
+                case 1 :
+                    borderInput2.style.setProperty("--visibleNumber", "inline");
+                    borderInput2.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+                    break
+                case 2 :
+                    alert2.style.setProperty("display", "inline");
+                    borderInput3.style.setProperty("--visibleNumber", "inline")
+                    borderInput3.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+                    break
+                case 3 :
+                    alert2.style.setProperty("display", "inline");
+                    borderInput4.style.setProperty("--visibleNumber", "inline")
+                    borderInput4.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+                    break
+                case 4 :
+                    alert3.style.setProperty("display", "inline");
+                    borderInput5.style.setProperty("--visibleNumber", "inline")
+                    borderInput5.style.setProperty("--borderColor", "hsl(0, 100%, 66%)");
+                    break
+                default:
+                    console.log(cont);
+            };
+        });
+        }
+})
+
+/******************************* button continue *************************/
+const carryON = document.getElementById("continue");
+
+carryON.addEventListener("click", ()=>{
+    location.reload()
+})
